a9bd8bb2ec0e7be8c4afe7f8b0fede854d008241
Bulb1 <bartoszkawalec90@gmail.com>
2024-11-10 21:24:56 +0100

    - added Swagger integration for API documentation

--- src/main/java/com/wavebank/wave/user/UserController.java
+++ src/main/java/com/wavebank/wave/user/UserController.java
@@ -1,5 +1,8 @@
 package com.wavebank.wave.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +13,27 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Tag(name = "User")
 
 public class UserController {
 
     private final UserService userService;
+
+    @Operation(
+            description = "Get enpoint for user",
+            summary = "This is a summary for user get endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403"
+                    )
+            }
+    )
+
     @GetMapping
     public List<User> getUsers(){
         return userService.getUsers();
